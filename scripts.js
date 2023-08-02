ddade6d6c2186c39d9e167b1fd56e4a37357d338
jackpmcd <jackpanos@gmail.com>
2023-08-02 12:55:05 +0100

    edited the javascript

--- scripts.js
+++ scripts.js
@@ -1,18 +1,3 @@
-var request = new XMLHttpRequest()
-
-request.open('GET', 'https://jsonplaceholder.typicode.com/jackpmcd/API-practice', true)
-
-request.onload = function () {
-    var data = JSON.parse(this.response)
-
-    if (request.status >= 200 && request.status < 400){
-        data.forEach((movie) => {
-            console.log(movie.title)
-        })
-    } else{
-        console.log('error')
-    }
-}
-
-request.send()
-
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(data => console.log(data))
